0d27d6152a1019638176740a343a25bd9d17b860
thu <sadar19919@gmail.com>
2018-01-08 08:46:12 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+$mang = array(1, 7,2,3,9,100,55,33); 
+  
+$count = count($mang);
+  
+// Sắp xếp mảng
+for ($i = 0; $i < ($count - 1); $i++)
+{
+    for ($j = $i + 1; $j < $count; $j++) 
+    {
+        if ($mang[$i] > $mang[$j]) 
+        {
+            $tmp = $mang[$j];
+            $mang[$j] = $mang[$i];
+            $mang[$i] = $tmp;
+        }
+    }
+}
+
+print_r($mang);
+ ?>
